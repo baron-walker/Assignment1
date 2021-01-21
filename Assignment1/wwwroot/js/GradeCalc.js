24699df369697552b6c65a4d88013bec2b388de9
Brian Walker <tomailbrian@gmail.com>
2021-01-20 18:52:50 -0700

    First web app

---
+++ Assignment1/wwwroot/js/GradeCalc.js
@@ -0,0 +1,38 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+    grade = (((document.getElementById("ass").value * .5) +
+        (document.getElementById("group").value * .1) +
+        (document.getElementById("quiz").value * .1) +
+        (document.getElementById("exam").value * .2) +
+        (document.getElementById("int").value * .1)).toFixed(3) * 100);
+
+    letter = "A";
+    if (grade >= 94) {
+        letter = "A";
+    } else if (grade >= 90) {
+        letter = "A-";
+    } else if (grade >= 87) {
+        letter = "B+";
+    } else if (grade >= 84) {
+        letter = "B";
+    } else if (grade >= 80) {
+        letter = "B-";
+    } else if (grade >= 77) {
+        letter = "C+";
+    } else if (grade >= 74) {
+        letter = "C";
+    } else if (grade >= 70) {
+        letter = "C-";
+    } else if (grade >= 67) {
+        letter = "D+";
+    } else if (grade >= 64) {
+        letter = "D";
+    } else if (grade >= 60) {
+        letter = "D-";
+    } else {
+        letter = "E";
+    }
+
+    alert("Your total grade would be: " + grade + "%" + "\nYour letter grade would be a(n): " + letter);
+});
